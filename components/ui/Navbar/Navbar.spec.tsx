4a684ff27be4892de3684b2e41448cfaffdc579d
Luis Melendez <melendez12341@gmail.com>
2022-07-07 18:59:29 -0400

    fix: test Navbar

--- components/ui/Navbar/Navbar.spec.tsx
+++ components/ui/Navbar/Navbar.spec.tsx
@@ -1,1 +1,9 @@
+import { render } from "@testing-library/react";
 import { Navbar } from "./Navbar";
+
+describe("<Navbar />", () => {
+    it("should render", () => {
+        const { getByText } = render(<Navbar />);
+        expect(getByText("Open Jira")).toBeInTheDocument();
+    });
+});
